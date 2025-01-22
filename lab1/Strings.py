081231c660474509b24577c4d24958226226147b
keyara1 <bekarysperdebaev7@gmail.com>
2025-01-23 02:30:39 +0500

    update

---
+++ lab1/Strings.py
@@ -0,0 +1,25 @@
+#Strings
+print("Hello")
+print('Hello')
+
+#Slicing Strings
+b = "Hello, World!"
+print(b[2:5])
+
+#Modify Strings
+a = "Hello, World!"
+print(a.upper())
+
+#String Concatenation
+a = "Hello"
+b = "World"
+c = a + b
+print(c)
+
+#String Format
+age = 36
+txt = f"My name is John, I am {age}"
+print(txt)
+
+#Escape Character
+txt = "We are the so-called \"Vikings\" from the north."
