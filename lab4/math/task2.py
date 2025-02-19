2ee3d4d4947cc23ddadf8c1f8387b2a30f1912b3
beksh <bekarysperdebaev7@gmail.com>
2025-02-19 13:07:04 +0500

    Update

--- lab4/math/task2.py
+++ lab4/math/task2.py
@@ -7,11 +7,4 @@ second_base = int(input("Enter height: "))
 midle = (first_base + second_base) / 2
 area  = midle * height
 
-del height
-del first_base
-del second_base
-del midle
-
 print(f"Area of trapezoid is {area}")
-
-del area
